598f2296005b31f72560872230e2ba5d2683bbcb
debian <debian@debian.debian>
2025-12-24 15:11:07 +0100

    test pingora load balancer with local services and without TLS

--- pingora/src/main.rs
+++ pingora/src/main.rs
@@ -24,31 +24,35 @@ impl ProxyHttp for LB {
 
         println!("upstream peer is: {} (weight = {})", upstream.addr, upstream.weight);
 
-        let peer = Box::new(HttpPeer::new(upstream, true, "one.one.one.one".to_string()));
+        let peer = Box::new(HttpPeer::new(upstream, false, "".to_string()));
         Ok(peer)
     }
 
-    async fn upstream_request_filter(
-        &self,
-        _session: &mut Session,
-        upstream_request: &mut pingora_http::RequestHeader,
-        _ctx: &mut Self::CTX,
-    ) -> Result<()> {
-        upstream_request
-            .insert_header("Host", "one.one.one.one")?;
-        Ok(())
-    }
+    // async fn upstream_request_filter(
+    //     &self,
+    //     _session: &mut Session,
+    //     upstream_request: &mut pingora_http::RequestHeader,
+    //     _ctx: &mut Self::CTX,
+    // ) -> Result<()> {
+    //     upstream_request
+    //         .insert_header("Host", "one.one.one.one")?;
+    //     Ok(())
+    // }
 }
 
 fn main() {
+    let lb_address = "0.0.0.0:3000";
+    let upstream_sockets = ["0.0.0.0:8080", "0.0.0.0:8081", "0.0.0.0:777"];
+    println!("Running load balancer at {lb_address}");
+    println!("Upstreams: {upstream_sockets:?}");
+
     // read command line arguments
     let opt = Opt::parse_args();
     let mut my_server = Server::new(Some(opt)).unwrap();
     my_server.bootstrap();
 
-    // 127.0.0.1:343" is just a bad server
     let mut upstreams =
-        LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443", "127.0.0.1:343"]).unwrap();
+        LoadBalancer::try_from_iter(upstream_sockets).unwrap();
 
     // We add health check in the background so that the bad server is never selected.
     let hc = health_check::TcpHealthCheck::new();
@@ -60,14 +64,15 @@ fn main() {
     let upstreams = background.task();
 
     let mut lb = pingora_proxy::http_proxy_service(&my_server.configuration, LB(upstreams));
+    lb.add_tcp(lb_address);
 
-    let cert_path = format!("{}/tls/server.crt", env!("CARGO_MANIFEST_DIR"));
-    let key_path = format!("{}/tls/key.pem", env!("CARGO_MANIFEST_DIR"));
+    // let cert_path = format!("{}/tls/server.crt", env!("CARGO_MANIFEST_DIR"));
+    // let key_path = format!("{}/tls/key.pem", env!("CARGO_MANIFEST_DIR"));
 
-    let mut tls_settings =
-        pingora_core::listeners::tls::TlsSettings::intermediate(&cert_path, &key_path).unwrap();
-    tls_settings.enable_h2();
-    lb.add_tls_with_settings("0.0.0.0:6189", None, tls_settings);
+    // let mut tls_settings =
+    //     pingora_core::listeners::tls::TlsSettings::intermediate(&cert_path, &key_path).unwrap();
+    // tls_settings.enable_h2();
+    // lb.add_tls_with_settings(lb_address, None, tls_settings);
 
     my_server.add_service(lb);
     my_server.add_service(background);
