c5f87adab1d6440d24d6e76e4bf8a119a4591681
GyulyVGC <gyulyvgc99@gmail.com>
2026-01-12 15:41:48 +0100

    get upstreams based on client IP plus target service

--- nullnet-proxy-test/src/main.rs
+++ nullnet-proxy-test/src/main.rs
@@ -6,13 +6,16 @@ use pingora_core::{Error, ErrorType, Result};
 use pingora_proxy::{ProxyHttp, Session};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::sync::Arc;
 use std::sync::Mutex;
 
 pub struct NullnetProxy {
-    /// Mapping of client IPs to upstream service addresses
-    cs_map: Arc<Mutex<HashMap<IpAddr, SocketAddr>>>,
+    /// The available services and their host machine addresses
+    services: HashMap<Service, SocketAddr>,
+    /// Mapping of client IP + target service to upstream VLAN address
+    connections: Arc<Mutex<HashMap<ClientRequest, SocketAddr>>>,
     /// Last registered VLAN ID
     last_registered_vlan: Arc<Mutex<u16>>,
     /// UDP socket for sending VLAN setup requests
@@ -21,8 +24,21 @@ pub struct NullnetProxy {
 
 impl NullnetProxy {
     pub fn new() -> Self {
+        // define available services and their host addresses
+        // this will eventually be loaded from a config file or database
+        let mut services = HashMap::new();
+        services.insert(
+            Service("color.com".to_string()),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 104)), 3001),
+        );
+        services.insert(
+            Service("directory.com".to_string()),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 104)), 8080),
+        );
+
         Self {
-            cs_map: Arc::new(Mutex::new(HashMap::new())),
+            services,
+            connections: Arc::new(Mutex::new(HashMap::new())),
             last_registered_vlan: Arc::new(Mutex::new(100)),
             udp_socket: Arc::new(
                 UdpSocket::bind("0.0.0.0:9997").expect("Failed to bind UDP socket"),
@@ -30,12 +46,16 @@ impl NullnetProxy {
         }
     }
 
-    pub fn get_or_add_upstream(&self, client_ip: IpAddr) -> Option<SocketAddr> {
-        if let Some(upstream) = self.cs_map.lock().ok()?.get(&client_ip) {
+    pub fn get_or_add_upstream(&self, client_req: ClientRequest) -> Option<SocketAddr> {
+        if let Some(upstream) = self.connections.lock().ok()?.get(&client_req) {
             return Some(*upstream);
         }
 
-        println!("Setting up new upstream for client {client_ip}...");
+        println!("Setting up new upstream for {client_req}");
+
+        let host = self.services.get(&client_req.service)?;
+        let host_ip = host.ip();
+        let host_port = host.port();
 
         let vlan_id = {
             let mut last_id = self.last_registered_vlan.lock().ok()?;
@@ -48,22 +68,19 @@ impl NullnetProxy {
         let port_ip = Ipv4Addr::new(10, a, b, 2);
         let ipv4_network = Ipv4Network::new(port_ip, 24).ok()?;
         self.send_vlan_setup_request(
+            // this machine's IP
             IpAddr::V4(Ipv4Addr::new(192, 168, 1, 130)),
             vlan_id,
             vec![ipv4_network],
         )?;
 
-        // create dedicated VLAN on webserver and get its upstream address
+        // create dedicated VLAN on target host and get newly created upstream address
         let port_ip = Ipv4Addr::new(10, a, b, 1);
         let ipv4_network = Ipv4Network::new(port_ip, 24).ok()?;
-        self.send_vlan_setup_request(
-            IpAddr::V4(Ipv4Addr::new(192, 168, 1, 104)),
-            vlan_id,
-            vec![ipv4_network],
-        )?;
+        self.send_vlan_setup_request(host_ip, vlan_id, vec![ipv4_network])?;
 
-        let upstream = SocketAddr::new(IpAddr::V4(port_ip), 3001);
-        self.cs_map.lock().ok()?.insert(client_ip, upstream);
+        let upstream = SocketAddr::new(IpAddr::V4(port_ip), host_port);
+        self.connections.lock().ok()?.insert(client_req, upstream);
 
         // wait a bit for VLAN setup to complete
         std::thread::sleep(std::time::Duration::from_secs(1));
@@ -94,6 +111,11 @@ impl ProxyHttp for NullnetProxy {
     fn new_ctx(&self) -> Self::CTX {}
 
     async fn upstream_peer(&self, session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+        let uri = session
+            .req_header()
+            .uri
+            .host()
+            .ok_or_else(|| Error::explain(ErrorType::BindError, "No host in request URI"))?;
         let client_ip = session
             .client_addr()
             .ok_or_else(|| {
@@ -108,10 +130,13 @@ impl ProxyHttp for NullnetProxy {
             })?
             .ip();
 
+        let service = Service(uri.to_string());
+        let client_req = ClientRequest { client_ip, service };
+        println!("{client_req}");
         let upstream = self
-            .get_or_add_upstream(client_ip)
+            .get_or_add_upstream(client_req)
             .ok_or_else(|| Error::explain(ErrorType::BindError, "Failed to retrieve upstream"))?;
-        println!("client: {client_ip}\nupstream: {upstream}\n");
+        println!("upstream: {upstream}\n");
 
         let peer = Box::new(HttpPeer::new(upstream, false, String::new()));
         Ok(peer)
@@ -133,6 +158,21 @@ fn main() {
     my_server.add_service(proxy);
     my_server.run_forever();
 }
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct ClientRequest {
+    pub client_ip: IpAddr,
+    pub service: Service,
+}
+
+impl Display for ClientRequest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -> {}", self.client_ip, self.service.0)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Service(String);
 
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Default)]
 pub struct OvsVlan {
